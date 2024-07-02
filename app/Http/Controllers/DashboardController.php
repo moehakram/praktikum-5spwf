263ad3e0c0eb3d3af4171eafc9302db940e47979
moehakram <muh.akram1422@gmail.com>
2024-07-02 14:46:51 +0800

    refactoring code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,19 +7,28 @@ use App\Models\Organisasi;
 use App\Models\Peminjaman;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        if($request->user() === null){
+       
+        if(($user = Auth::user()) === null){
             return redirect()->route('login');
         }else{
+            if($user->hasRole('admin')){
+                $total_pinjam = Peminjaman::sum('jumlah');
+                $tot_brg_belum_kembali = Peminjaman::doesntHave('pengembalian')->sum('jumlah');
+            }else{
+                $total_pinjam = Peminjaman::with(['inventaris', 'pengurus'])->doesntHave('pengembalian')->where('pengurus_id', $id = Auth::id())->sum('jumlah');
+                $tot_brg_belum_kembali = Peminjaman::doesntHave('pengembalian')->where('pengurus_id', $id)->sum('jumlah');
+            }
             return view('admin.dashboard', [
                 'tot_pegawai' => User::count() - 1, 
-                'tot_pinjam' => Peminjaman::sum('jumlah'), 
+                'tot_pinjam' => $total_pinjam,
                 'jumlah_organisasi' => Organisasi::count(),
-                'tot_brg_belum_kembali' => Peminjaman::doesntHave('pengembalian')->sum('jumlah'), 
+                'tot_brg_belum_kembali' => $tot_brg_belum_kembali 
             ]);
         }
     }
