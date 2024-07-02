d33038efb7214e15c1a760fd262afb610b3a28c0
moehakram <muh.akram1422@gmail.com>
2024-07-02 13:11:26 +0800

    add new layout app

---
+++ resources/views/layouts/_app.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="/viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ $title ?? 'APLIKASI INVENTARIS' }}</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('./vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('./vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('./vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('./vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('./vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/js/select.dataTables.min.css') }}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('./css/vertical-layout-light/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('./images/logo-new.png') }}" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
+    @yield('style')
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/app-navbar.blade.php -->
+       @include('partials/app-navbar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/app-sidebar.blade.php -->
+            @include('partials/app-sidebar')
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                  @yield('content')
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/app-footer.blade.php -->
+               @include('partials/app-footer')
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+<script src="{{ asset('./vendors/js/vendor.bundle.base.js') }}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+{{-- <script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script> --}}
+<script src="{{ asset('vendors/datatables.net/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('js/dataTables.select.min.js') }}"></script>
+
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{ asset('./js/off-canvas.js') }}"></script>
+<script src="{{ asset('./js/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('./js/template.js') }}"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+<!-- endinject -->
+
+@yield('script')
+
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@if (session()->has('alert'))
+    <script>
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+          });
+          Toast.fire({
+            icon: "{{ session('alert')}}",
+            title: "{{ session('message')}}"
+          });
+    </script>
+@endif
+
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,94 +2,184 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="/viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>{{ $title ?? 'APLIKASI INVENTARIS' }}</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="{{ asset('./vendors/feather/feather.css') }}">
-    <link rel="stylesheet" href="{{ asset('./vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('./vendors/css/vendor.bundle.base.css') }}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('./vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-    <link rel="stylesheet" href="{{ asset('./vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/js/select.dataTables.min.css') }}">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('./css/vertical-layout-light/style.css') }}">
-    <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('./images/logo-new.png') }}" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
-    @yield('style')
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>{{ $title ?? 'APLIKASI INVENTARIS' }}</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{ asset('vendors/feather/feather.css')}}">
+  <link rel="stylesheet" href="{{ asset('vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css')}}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+  <link rel="stylesheet" href="{{ asset('vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('js/select.dataTables.min.css')}}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css')}}">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{ asset('./images/logo-new.png') }}"/>
 </head>
-
 <body>
-    <div class="container-scroller">
-        <!-- partial:partials/app-navbar.blade.php -->
-       @include('partials/app-navbar')
-        <!-- partial -->
-        <div class="container-fluid page-body-wrapper">
-            <!-- partial:partials/app-sidebar.blade.php -->
-            @include('partials/app-sidebar')
-            <!-- partial -->
-            <div class="main-panel">
-                <div class="content-wrapper">
-                  @yield('content')
-                </div>
-                <!-- content-wrapper ends -->
-                <!-- partial:partials/app-footer.blade.php -->
-               @include('partials/app-footer')
-                <!-- partial -->
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="{{ route('home') }}">INVENTARIS</a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <img src="{{ asset('images/logo-new.png') }}" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item" href="{{route('profile')}}">
+                <i class="ti-settings text-primary"></i>
+                update profile
+              </a>
+              <a class="dropdown-item" href="{{route('password')}}">
+                <i class="ti-settings text-primary"></i>
+                update password
+              </a>
+              <a class="dropdown-item" href="{{route('logout')}}">
+                <i class="ti-power-off text-primary"></i>
+                Logout
+              </a>
             </div>
-            <!-- main-panel ends -->
+          </li>
+          {{-- <li class="nav-item nav-settings d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              <i class="icon-ellipsis"></i>
+            </a>
+          </li> --}}
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">     
+      <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('home')}}">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('inventaris.index')}}">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Inventaris</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Transaksi</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="{{route('peminjaman.index')}}">Peminjaman Aset</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{route('pengembalian.index')}}">Pengembalian Aset</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{route('laporan')}}">Laporan</a></li>
+              </ul>
+            </div>
+          </li>
+          {{-- @haspermission('pagePegawai') --}}
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('organisasi.index')}}">
+              <i class="icon-grid-2 menu-icon"></i>
+              <span class="menu-title">Organisasi</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('pegawai.index')}}">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Pegawai</span>
+            </a>
+          </li>
+          {{-- @endhaspermission --}}
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">Welcome Aamir</h3>
+                  <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span class="text-primary">3 unread alerts!</span></h6>
+                </div>
+                <div class="col-12 col-xl-4">
+                 <div class="justify-content-end d-flex">
+                  <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                    <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                     <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
+                      <a class="dropdown-item" href="#">January - March</a>
+                      <a class="dropdown-item" href="#">March - June</a>
+                      <a class="dropdown-item" href="#">June - August</a>
+                      <a class="dropdown-item" href="#">August - November</a>
+                    </div>
+                  </div>
+                 </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </div>
-        <!-- page-body-wrapper ends -->
-    </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-<script src="{{ asset('./vendors/js/vendor.bundle.base.js') }}"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-{{-- <script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script> --}}
-<script src="{{ asset('vendors/datatables.net/jquery.dataTables.js') }}"></script>
-<script src="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-<script src="{{ asset('js/dataTables.select.min.js') }}"></script>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024.  my tugas akhir</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Ilyas & Rifki</span>
+          </div>
+        </footer> 
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>   
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
 
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="{{ asset('./js/off-canvas.js') }}"></script>
-<script src="{{ asset('./js/hoverable-collapse.js') }}"></script>
-<script src="{{ asset('./js/template.js') }}"></script>
-{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
-<!-- endinject -->
+  <!-- plugins:js -->
+  <script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{asset('vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('vendors/datatables.net/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+  <script src="{{asset('js/dataTables.select.min.js')}}"></script>
 
-@yield('script')
-
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-@if (session()->has('alert'))
-    <script>
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-          }
-          });
-          Toast.fire({
-            icon: "{{ session('alert')}}",
-            title: "{{ session('message')}}"
-          });
-    </script>
-@endif
-
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{asset('js/off-canvas.js')}}"></script>
+  <script src="{{asset('js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('js/template.js')}}"></script>
+  <script src="{{asset('js/settings.js')}}"></script>
+  <script src="{{asset('js/todolist.js')}}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{asset('js/dashboard.js')}}"></script>
+  <script src="{{asset('js/Chart.roundedBarCharts.js')}}"></script>
+  <!-- End custom js for this page-->
 </body>
 
 </html>
+
